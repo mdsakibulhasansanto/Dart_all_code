eccc0e4fa8b7de4f144e4a2346dbe0439d9521c2
md_santo <mdsakibulhasansanto7@gmail.com>
2024-09-22 00:10:59 +0600

    update

---
+++ all_file/inheritance_oop.dart
@@ -0,0 +1,29 @@
+
+// first_class
+class FirstClass {
+
+  String studentName = 'Md Sakibul hasan';
+
+  void deposit (){
+    int deposit = 500;
+  }
+}
+
+// second_class
+class Second extends FirstClass{
+  @override
+  void deposit (){
+    int deposit = 1000;
+    print(deposit);
+  }
+}
+
+// Main function
+
+void main (){
+
+
+  Second second = Second();
+  print(second.studentName);
+  second.deposit();
+}
