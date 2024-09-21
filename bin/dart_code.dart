e4f0314ee367a83ff0d0a5b4e980bc809c4d5986
md_santo <mdsakibulhasansanto7@gmail.com>
2024-09-21 23:28:10 +0600

    first

---
+++ bin/dart_code.dart
@@ -0,0 +1,5 @@
+import 'package:dart_code/dart_code.dart' as dart_code;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_code.calculate()}!');
+}
---
+++ test/dart_code_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_code/dart_code.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
