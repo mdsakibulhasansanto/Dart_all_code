d9b038b1a437dab6a57f2baa92278380e9a4795e
md_santo <mdsakibulhasansanto7@gmail.com>
2024-09-21 23:51:44 +0600

    update

---
+++ all_file/while_loop.dart
@@ -0,0 +1,19 @@
+void main(){
+
+  /* While loop =================
+  int x = 0;
+  while(x<10){
+    print(x);
+    x++;
+  }
+
+   */
+
+  int i = 0;
+  do{
+    print('Santo');
+    i++;
+  }while(i<20);
+
+
+}
